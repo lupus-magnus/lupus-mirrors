35a1116cd9b5c3988a82baa0fe3aeb10aa0130bc
Lobo <matheuscardoso1@poli.ufrj.br>
2021-01-24 22:50:29 -0300

    functional navbar and switch

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import styles from './Nav.module.css';
 import React, { Component } from 'react';
 import switchOn from '../imgs/switchOn2.png';
 import switchOff from '../imgs/switchOff.png';
+import { motion } from 'framer-motion';
 
 class Nav extends Component {
     state = { displayRemote: this.props.display }
@@ -15,13 +16,18 @@ class Nav extends Component {
         console.log('props:',this.props)
         console.log('state:',this.state)
         return ( 
-            <div className={styles.navbar}>
+            <motion.div className={styles.navbar}
+                initial={{alpha: 1}}
+                animate={{opacity: 0.05}}
+                transition={{delay: 0.5, duration: 1}}
+                whileHover={{opacity: 1, transition: {}}}
+            >
                 <div className={styles.switchContainer}>
                     <p>Remote Control: {this.state.displayRemote ? 'ON' : 'OFF'}</p>
                     <img alt='switch' onClick={() => this.toggleDisplay()} className={styles.switch} src={this.state.displayRemote ? switchOn : switchOff }/>
                 </div>
             
-            </div>
+            </motion.div>
          );
     }
 }
