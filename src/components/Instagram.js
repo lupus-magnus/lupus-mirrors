1c34bc0398a7fccec368dd19926521c5957f21c8
Lobo <matheuscardoso1@poli.ufrj.br>
2021-02-09 13:22:31 -0300

    trying to adjust netflix mobile view

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import Remote from './remote';
+import InstagramPage from './clones/instagramPage';
 import { motion } from 'framer-motion';
 
 class Instagram extends Component {
@@ -7,7 +7,9 @@ class Instagram extends Component {
     render() { 
         return ( <motion.div
             exit={{opacity: 0, transition: {duration: 1.3}}}
-        > <h1>Instagram!</h1> </motion.div>  );
+        >
+            <InstagramPage />
+        </motion.div>  );
     }
 }
  
---
+++ src/components/clones/instagramPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class InstagramPage extends Component {
+    render() { 
+        return ( 
+            <h1>oie :D</h1>
+         );
+    }
+}
+ 
+export default InstagramPage; 
